d945021686a62a8bb709e9b2c3d052b74a119a85
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-11-25 09:38:26 +0700

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -20,10 +20,7 @@ sed -i 's/^#Para/Para/' /etc/pacman.conf
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 pacman -Sy --noconfirm
 
-sed -i 'pc' /etc/hostname
-sed -i '127.0.0.1 localhost' /etc/hosts
-sed -i '::1 localhost' /etc/hosts
-sed -i '127.0.1.1 pc.localhost pc' /etc/hosts
-
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd 
-yay -Sy --noconfirm grub networkmanager network-manager-applet linux-headers bluez cups xdg-utils xdg-user-dirs exfatprogs
+yay -Sy --noconfirm --no-ask-password grub networkmanager network-manager-applet linux-headers bluez cups xdg-utils xdg-user-dirs exfatprogs linux intel-ucode xorg xf86-video-intel sddm plasma xfce4-goodies awesome-terminal-fonts nerd-fonts-source-code-pro ttf-anonymous-pro ttf-bitstream-vera ttf-caladea ttf-carlito ttf-cascadia-code ttf-cormorant ttf-croscore ttf-eurof
+ttf-fantasque-sans-mono ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-font-awesome ttf-hector ttf-hellvetica ttf-ibm-plex ttf-inconsolata ttf-jetbrains-mono ttf-joypixels
+ttf-lato ttf-liberation ttf-linux-libertine ttf-linux-libertine-g ttf-meslo-nerd-font-powerlevel10k ttf-monofur ttf-ms-fonts ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono ttf-opensans ttf-proggy-clean opera adapta-gtk-theme materia-gtk-theme breeze materia-kde ventoy-bin 
