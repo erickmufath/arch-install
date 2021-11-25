1f7b52603e5c61ca20bc5debd6e561192ab5d1ab
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-11-25 11:26:34 +0700

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -23,8 +23,3 @@ sed -i 's/^#Para/Para/' /etc/pacman.conf
 #Enable multilib
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 pacman -Sy --noconfirm
-
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd 
-yay -Sy --noconfirm --no-ask-password grub networkmanager network-manager-applet linux-headers bluez cups xdg-utils xdg-user-dirs exfatprogs linux intel-ucode xorg xf86-video-intel sddm plasma xfce4-goodies awesome-terminal-fonts nerd-fonts-source-code-pro ttf-anonymous-pro ttf-bitstream-vera ttf-caladea ttf-carlito ttf-cascadia-code ttf-cormorant ttf-croscore ttf-eurof
-ttf-fantasque-sans-mono ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-font-awesome ttf-hector ttf-hellvetica ttf-ibm-plex ttf-inconsolata ttf-jetbrains-mono ttf-joypixels
-ttf-lato ttf-liberation ttf-linux-libertine ttf-linux-libertine-g ttf-meslo-nerd-font-powerlevel10k ttf-monofur ttf-ms-fonts ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono ttf-opensans ttf-proggy-clean opera adapta-gtk-theme materia-gtk-theme breeze materia-kde ventoy-bin sddm-nordic-theme-git gst-plugin-pipewire libpipewire02 pipewire pipewire-alsa pipewire-jack pipewire-pulse pipewire-zeroconf wireplumber lib32-pipewire lib32-pipewire-jack
