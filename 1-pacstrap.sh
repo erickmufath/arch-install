e26283b1c5481109a8f25f5399f0742a7f9207a6
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-07 18:44:18 +0700

    Update 1-pacstrap.sh

--- 1-pacstrap.sh
+++ 1-pacstrap.sh
@@ -8,6 +8,6 @@ echo "--------------------------------------"
 echo -e "\nPacstrap Dimulai...\n$HR"
 echo "--------------------------------------"
 #script
-pacstrap /mnt base base-devel linux linux-firmware linux-headers nano sudo archlinux-keyring wget libnewt intel-ucode ntfsprogs ntfs-3g dosfstools dos2unix e2fsprogs xfsprogs btrfs-progs --noconfirm --needed
+pacstrap /mnt base base-devel linux linux-firmware linux-headers nano sudo archlinux-keyring wget git libnewt intel-ucode ntfsprogs ntfs-3g dosfstools dos2unix e2fsprogs xfsprogs btrfs-progs --noconfirm --needed
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
