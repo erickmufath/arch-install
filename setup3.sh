a91f179c0bf63045d438f5c27c5241e45bfe48ae
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-07 20:12:49 +0700

    Update setup3.sh

--- setup3.sh
+++ setup3.sh
@@ -1,6 +1,6 @@
 chmod +x arch-install/3-install-pkg.sh
-chmod +x arch-install/4-enable-service.sh
 sh arch-install/3-install-pkg.sh
 echo "Masukkan Root Password"
+chmod +x arch-install/4-enable-service.sh
 sudo su
 sh arch-install/4-enable-service.sh
