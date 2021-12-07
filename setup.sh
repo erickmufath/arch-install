81263137c480315ac858e89533a615e87508a323
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-07 18:27:15 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
+pacman -Sy sed --noconfirm --needed
+#Add parallel downloading
+sed -i 's/^#Para/Para/' /etc/pacman.conf
+#Enable multilib
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+pacman -Syu --noconfirm
 chmod +x arch-install/1-pacstrap.sh
 sh arch-install/1-pacstrap.sh
