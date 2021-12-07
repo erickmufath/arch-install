9f4c3bad09d8dd1cdf3d8168aedabdbef3350194
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-07 17:51:06 +0700

    Update 2-chroot.sh

--- 2-chroot.sh
+++ 2-chroot.sh
@@ -1,2 +1,6 @@
 chmod +x arch-install/3-preinstall-pkg.sh
 sh arch-install/3-preinstall-pkg.sh
+chmod +x arch-install/4-exit.sh
+sh arch-install/4-exit.sh
+chmod +x arch-install/5-reboot.sh
+sh arch-install/5-reboot.sh
