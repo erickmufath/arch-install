ec99ad7ee089588497cdf28b655323842e1f96df
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-08 09:47:39 +0000

    Update setup4.sh

--- setup4.sh
+++ setup4.sh
@@ -1,3 +1,5 @@
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd ..
+sudo pacman -Syu --needed && sudo pacman-key --refresh-keys --needed && sudo gpg --refresh-keys --needed
 chmod +x arch-install/yay.sh
 sh yay.sh
 echo "--------Setup Keempat Selesai--------"
