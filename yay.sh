c3e9c6dab09220975dbadacb725a665bde585d10
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-09 16:11:48 +0700

    Update yay.sh

--- yay.sh
+++ yay.sh
@@ -1,10 +1,13 @@
-yay -S rar
-yay -S sndcpy
-yay -S ventoy-bin
-yay -S sddm-nordic-theme-git
-yay -S pipewire-jack-replacement
-yay -S dxvk-bin
-yay -S aic94xx-firmware
-yay -S wd719x-firmware
-yay -S upd72020x-fw
-yay -S nerd-fonts-source-code-pro
+arch-chroot /mnt git clone https://aur.archlinux.org/yay.git
+arch-chroot /mnt cd yay
+arch-chroot /mnt makepkg -si
+arch-chroot /mnt yay -S rar
+arch-chroot /mnt yay -S sndcpy
+arch-chroot /mnt yay -S ventoy-bin
+arch-chroot /mnt yay -S sddm-nordic-theme-git
+arch-chroot /mnt yay -S pipewire-jack-replacement
+arch-chroot /mnt yay -S dxvk-bin
+arch-chroot /mnt yay -S aic94xx-firmware
+arch-chroot /mnt yay -S wd719x-firmware
+arch-chroot /mnt yay -S upd72020x-fw
+arch-chroot /mnt yay -S nerd-fonts-source-code-pro
