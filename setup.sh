5542460eb87831b3bad67c724cbab5ea0a50e6ce
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-09 17:57:55 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,11 @@ chmod +x arch-install/*
 cp -rf arch-install /mnt/home/${usrname}/
 pacman -Sy grub efibootmgr --noconfirm --needed
 clear
+echo "======================================================="
 echo "=] 1. BIOS/Legacy"
 echo "=] 2. UEFI"
 echo "=] 3. HYBRID [Direkomendasikan Untuk Media External]"
+echo "======================================================="
 read -p ">> Pilih Jenis Boot (1/2/3) : " boot
 case $boot in
 1)
