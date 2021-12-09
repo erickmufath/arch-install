ebd5a9ef07e9d158e3e7342a0a1f90bee81213ff
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-09 18:25:07 +0700

    Update legacy.sh

--- legacy.sh
+++ legacy.sh
@@ -1,5 +1,4 @@
-grub-install --target=i386-pc /dev/sda
-grub-mkconfig -o /mnt/boot/grub/grub.cfg
+arch-chroot /mnt grub-install --target=i386-pc /dev/sda
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 echo "->] Masukkan Root Password "
 arch-chroot /mnt passwd
