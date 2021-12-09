11fa2805803d923b7c0172f565b112224e4fcbcd
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-09 17:54:05 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,15 +9,15 @@ echo "=] 3. HYBRID [Direkomendasikan Untuk Media External]"
 read -p ">> Pilih Jenis Boot (1/2/3) : " boot
 case $boot in
 1)
-sh arch-install/1.sh
+sh arch-install/base.sh
 sh arch-install/legacy.sh
 ;;
 2)
-sh arch-install/1.sh
+sh arch-install/base.sh
 sh arch-install/uefi.sh
 ;;
 3)
-sh arch-install/1.sh
+sh arch-install/base.sh
 sh arch-install/hybrid.sh
 ;;
 esac
