502fa1fabf1a343f9ae2db2795905f9dd870af18
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-09 16:58:50 +0700

    Update yay.sh

--- yay.sh
+++ yay.sh
@@ -9,3 +9,18 @@ yay -S aic94xx-firmware
 yay -S wd719x-firmware
 yay -S upd72020x-fw
 yay -S nerd-fonts-source-code-pro
+echo    "--------------------------------------------------------"
+echo -e "       Enabling Login Display Manager"
+sudo systemctl enable sddm.service
+sudo echo -e "\nSetup SDDM Theme"
+sudo cat <<EOF > /etc/sddm.conf
+[Theme]
+Current=Nordic
+EOF
+sudo systemctl enable cups
+sudo systemctl enable ntpd.service
+sudo systemctl disable dhcpcd.service
+sudo systemctl stop dhcpcd.service
+sudo systemctl enable NetworkManager.service
+sudo systemctl enable bluetooth
+echo    "--------------------------------------------------------"
