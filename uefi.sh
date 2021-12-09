e806dcae4ec52a7edcc858882100abaae31d399e
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-09 16:36:43 +0700

    Create uefi.sh

---
+++ uefi.sh
@@ -0,0 +1,20 @@
+arch-chroot /mnt pacman -Sy efibootmgr --noconfirm
+grub-install --target=x86_64-efi --efi-directory=/boot --boot-directory=/boot
+arch-chroot /mnt pacman -S xf86-video-intel xf86-video-amdgpu xf86-video-ati xf86-video-vesa --noconfirm
+echo "->] Masukkan Root Password "
+arch-chroot /mnt passwd
+echo "--------------------------------------------------------"
+read -p "->] Masukkan Username :" usrname
+echo "--------------------------------------------------------"
+echo "->] Masukkan User Password "
+echo "--------------------------------------------------------"
+arch-chroot /mnt useradd -mG wheel ${usrname}
+arch-chroot /mnt passwd ${usrname}
+echo "--------------------------------------------------------"
+echo "                  Reboot Ulang..."
+echo "                  Harap Copot Bootable Media..."
+echo "--------------------------------------------------------"
+sleep 5
+rm -rf arch-install
+umount -R /mnt
+reboot
