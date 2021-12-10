023751c359f62766f27b2f11b2e1058805e156c3
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-10 12:44:12 +0700

    Update uefi.sh

--- uefi.sh
+++ uefi.sh
@@ -1,5 +1,6 @@
-grub-install --target=x86_64-efi --efi-directory=/mnt/boot --boot-directory=/mnt/boot
-grub-mkconfig -o /mnt/boot/grub/grub.cfg
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/mnt/boot --boot-directory=/mnt/boot
+arch-chroot /mnt grub-mkconfig -o /mnt/boot/grub/grub.cfg
+arch-chroot /mnt mkinitcpio -p linux
 arch-chroot /mnt pacman -S xf86-video-intel xf86-video-amdgpu xf86-video-ati xf86-video-vesa --noconfirm
 echo "->] Masukkan Root Password "
 arch-chroot /mnt passwd
