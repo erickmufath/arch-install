6985fddc1a2ba55d29ae1cbda5ac2f5e4f65ed73
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-10 18:05:25 +0700

    Update pulseaudio.sh

--- pulseaudio.sh
+++ pulseaudio.sh
@@ -1,2 +1,2 @@
-sudo pacman -S pulseaudio jack jack2 pavucontrol paman kmix pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-zeroconf --noconfirm --needed
+sudo pacman -S pulseaudio jack jack2 pavucontrol paman kmix pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-zeroconf pamixer pulsemixer --noconfirm --needed
 yay -S pulseeffects --needed
