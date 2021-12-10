ef4e00fe47066e1a02e2f6e11012cdadc3aacab8
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-10 19:24:11 +0700

    Delete bios directory

--- bios/setup2.sh
+++
@@ -1,4 +0,0 @@
-chmod +x arch-install/2-preinstall-pkg.sh
-sh arch-install/2-preinstall-pkg.sh
-systemctl enable NetworkManager
-echo "--------Setup Kedua Selesai--------"
--- bios/setup3.sh
+++
@@ -1,5 +0,0 @@
-chmod +x arch-install/3-install-pkg.sh
-sh arch-install/3-install-pkg.sh
-chmod +x arch-install/4-enable-service.sh
-sh arch-install/4-enable-service.sh
-echo "--------Setup Ketiga Selesai--------"
