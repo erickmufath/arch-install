be159dd512d653ee997212781c42d4df7dadc1ef
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-10 12:21:07 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,14 +12,17 @@ read -p ">> Pilih Jenis Boot (1/2/3) : " boot
 case $boot in
 1)
 sh arch-install/base.sh
+arch-chroot /mnt pacman -S grub --noconfirm
 sh arch-install/legacy.sh
 ;;
 2)
 sh arch-install/base.sh
+arch-chroot /mnt pacman -S grub efibootmgr --noconfirm
 sh arch-install/uefi.sh
 ;;
 3)
 sh arch-install/base.sh
+arch-chroot /mnt pacman -S grub efibootmgr --noconfirm
 sh arch-install/hybrid.sh
 ;;
 esac
