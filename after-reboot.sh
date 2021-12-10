36c7b0415979327e70f72112c2aa63d3b35a76b6
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-10 14:50:24 +0700

    Update after-reboot.sh

--- after-reboot.sh
+++ after-reboot.sh
@@ -1,6 +1,9 @@
 sudo pacman -Sy bluez ttf-caladea ttf-carlito ttf-dejavu cups xdg-utils xdg-user-dirs exfatprogs linux intel-ucode unzip ark xorg xf86-video-intel sddm plasma xfce4-terminal nautilus scrcpy mpv elisa awesome-terminal-fonts ttf-anonymous-pro ttf-bitstream-vera ttf-caladea ttf-carlito ttf-cascadia-code ttf-cormorant ttf-croscore ttf-eurof ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-font-awesome ttf-ibm-plex ttf-inconsolata ttf-jetbrains-mono ttf-joypixels ttf-liberation ttf-linux-libertine ttf-linux-libertine-g ttf-monofur ttf-nerd-fonts-symbols ttf-opensans ttf-proggy-clean opera chromium adapta-gtk-theme materia-gtk-theme breeze materia-kde alsa-utils dhcpcd kdenlive obs-studio audacity network-manager-applet system-config-printer nano sudo packagekit-qt5 archlinux-keyring wget git libnewt ntfsprogs ntfs-3g dosfstools dos2unix e2fsprogs xfsprogs btrfs-progs libfdk-aac celt lame libdca opencore-amr opus speex --noconfirm --needed
 sudo pacman -Syyu --noconfirm
 git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
 yay -S rar --needed
 yay -S sndcpy --needed
 yay -S ventoy-bin --needed
@@ -17,8 +20,8 @@ yay -S vorbis --needed
 echo    "--------------------------------------------------------"
 echo -e "       Enabling Login Display Manager"
 sudo systemctl enable sddm.service
-sudo echo -e "\nSetup SDDM Theme"
-sudo cat <<EOF > /etc/sddm.conf
+echo -e "\nSetup SDDM Theme"
+cat <<EOF > /etc/sddm.conf
 [Theme]
 Current=Nordic
 EOF
