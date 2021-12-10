cbea16b3762911ea413a2d6528ddd35168b4b21b
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-10 16:19:32 +0700

    Update after-reboot.sh

--- after-reboot.sh
+++ after-reboot.sh
@@ -1,5 +1,20 @@
 sudo pacman -Sy bluez ttf-caladea ttf-carlito ttf-dejavu cups xdg-utils xdg-user-dirs exfatprogs linux intel-ucode unzip ark xorg xf86-video-intel sddm plasma xfce4-terminal nautilus scrcpy mpv elisa awesome-terminal-fonts ttf-anonymous-pro ttf-bitstream-vera ttf-caladea ttf-carlito ttf-cascadia-code ttf-cormorant ttf-croscore ttf-eurof ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-font-awesome ttf-ibm-plex ttf-inconsolata ttf-jetbrains-mono ttf-joypixels ttf-liberation ttf-linux-libertine ttf-linux-libertine-g ttf-monofur ttf-nerd-fonts-symbols ttf-opensans ttf-proggy-clean opera chromium adapta-gtk-theme materia-gtk-theme breeze materia-kde alsa-utils dhcpcd kdenlive obs-studio audacity network-manager-applet system-config-printer nano sudo packagekit-qt5 archlinux-keyring wget git libnewt ntfsprogs ntfs-3g dosfstools dos2unix e2fsprogs xfsprogs btrfs-progs libfdk-aac celt lame libdca opencore-amr opus speex --noconfirm --needed
 sudo pacman -Syyu --noconfirm
+echo    "--------------------------------------------------------"
+echo -e "       Enabling Login Display Manager"
+sudo systemctl enable sddm.service
+echo -e "\nSetup SDDM Theme"
+cat <<EOF > /etc/sddm.conf
+[Theme]
+Current=Nordic
+EOF
+sudo systemctl enable cups
+sudo systemctl enable ntpd.service
+sudo systemctl disable dhcpcd.service
+sudo systemctl stop dhcpcd.service
+sudo systemctl enable NetworkManager.service
+sudo systemctl enable bluetooth
+echo    "--------------------------------------------------------"
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
@@ -17,21 +32,6 @@ yay -S liba52 --needed
 yay -S mad --needed
 yay -S musepack --needed
 yay -S vorbis --needed
-echo    "--------------------------------------------------------"
-echo -e "       Enabling Login Display Manager"
-sudo systemctl enable sddm.service
-echo -e "\nSetup SDDM Theme"
-cat <<EOF > /etc/sddm.conf
-[Theme]
-Current=Nordic
-EOF
-sudo systemctl enable cups
-sudo systemctl enable ntpd.service
-sudo systemctl disable dhcpcd.service
-sudo systemctl stop dhcpcd.service
-sudo systemctl enable NetworkManager.service
-sudo systemctl enable bluetooth
-echo    "--------------------------------------------------------"
 echo    "--------------------------------------------------------"
 echo    "=] 1. Pulseaudio [Default Kebanyakan Distro]"
 echo    "=] 2. Pipewire [Audio Controller Modern]"
