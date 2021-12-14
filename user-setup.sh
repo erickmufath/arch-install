a99f0134a158c8d7f87747bfcadab34f2772ccc2
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-14 10:44:57 +0700

    Update user-setup.sh

--- user-setup.sh
+++ user-setup.sh
@@ -1,17 +1,17 @@
-echo    "--------------------------------------------------------"
-echo    "=] 1. Install Paket Basic (Sangat Minimal)"
-echo    "=] 2. Install Paket Standard Minimal"
-echo    "--------------------------------------------------------"
-read -p "->] Pilih (1/2) : " usrr
-case $usrr in
-1)
-sudo pacman -Sy bluez ttf-caladea ttf-carlito ttf-dejavu flatpak cups xdg-utils xdg-user-dirs exfatprogs unzip ark xorg sddm plasma xfce4-terminal nautilus mpv awesome-terminal-fonts ttf-anonymous-pro ttf-bitstream-vera ttf-caladea ttf-carlito ttf-cascadia-code ttf-cormorant ttf-croscore ttf-eurof ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-font-awesome ttf-ibm-plex ttf-inconsolata ttf-jetbrains-mono ttf-joypixels ttf-liberation ttf-linux-libertine ttf-linux-libertine-g ttf-monofur ttf-nerd-fonts-symbols ttf-opensans ttf-proggy-clean firefox adapta-gtk-theme materia-gtk-theme breeze materia-kde dhcpcd network-manager-applet system-config-printer nano sudo packagekit-qt5 archlinux-keyring wget git libnewt ntfsprogs ntfs-3g dosfstools dos2unix e2fsprogs xfsprogs btrfs-progs libfdk-aac celt lame libdca opencore-amr opus speex --noconfirm --needed
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-sudo pacman -Syyu --noconfirm
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ..
+echo    "--------------------------------------------------------"
+echo    "=] 1. Install Paket Basic (Sangat Minimal)"
+echo    "=] 2. Install Paket Standard Minimal"
+echo    "--------------------------------------------------------"
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+read -p "->] Pilih (1/2) : " usrr
+case $usrr in
+1)
+sudo pacman -Sy bluez ttf-caladea ttf-carlito ttf-dejavu flatpak cups xdg-utils xdg-user-dirs exfatprogs unzip ark xorg sddm plasma xfce4-terminal nautilus mpv awesome-terminal-fonts ttf-anonymous-pro ttf-bitstream-vera ttf-caladea ttf-carlito ttf-cascadia-code ttf-cormorant ttf-croscore ttf-eurof ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-font-awesome ttf-ibm-plex ttf-inconsolata ttf-jetbrains-mono ttf-joypixels ttf-liberation ttf-linux-libertine ttf-linux-libertine-g ttf-monofur ttf-nerd-fonts-symbols ttf-opensans ttf-proggy-clean firefox adapta-gtk-theme materia-gtk-theme breeze materia-kde dhcpcd network-manager-applet system-config-printer nano sudo packagekit-qt5 archlinux-keyring wget git libnewt ntfsprogs ntfs-3g dosfstools dos2unix e2fsprogs xfsprogs btrfs-progs libfdk-aac celt lame libdca opencore-amr opus speex --noconfirm --needed
+sudo pacman -Syyu --noconfirm
 yay -S timeshift --needed
 yay -S sddm-nordic-theme-git --needed
 clear
@@ -34,12 +34,7 @@ echo    "--------------------------------------------------------"
 ;;
 2)
 sudo pacman -Sy bluez ttf-caladea ttf-carlito ttf-dejavu flatpak cups xdg-utils xdg-user-dirs exfatprogs intel-ucode unzip ark xorg xf86-video-intel sddm plasma xfce4-terminal nautilus scrcpy mpv elisa awesome-terminal-fonts ttf-anonymous-pro ttf-bitstream-vera ttf-caladea ttf-carlito ttf-cascadia-code ttf-cormorant ttf-croscore ttf-eurof ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-font-awesome ttf-ibm-plex ttf-inconsolata ttf-jetbrains-mono ttf-joypixels ttf-liberation ttf-linux-libertine ttf-linux-libertine-g ttf-monofur ttf-nerd-fonts-symbols ttf-opensans ttf-proggy-clean firefox adapta-gtk-theme materia-gtk-theme breeze materia-kde dhcpcd kdenlive obs-studio audacity network-manager-applet system-config-printer nano sudo packagekit-qt5 archlinux-keyring wget git libnewt ntfsprogs ntfs-3g dosfstools dos2unix e2fsprogs xfsprogs btrfs-progs libfdk-aac celt lame libdca opencore-amr opus speex --noconfirm --needed
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo pacman -Syyu --noconfirm
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ..
 clear
 echo    "--------------------------------------------------------"
 echo    "=] 1. Pulseaudio [Default Kebanyakan Distro]"
