7abd3bd27ef38e967c0554db06384c5cc557483d
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-14 20:29:57 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,10 @@ chmod +x arch-install/*
 echo    "-]-]-]-]-]-]-]-]-]-]-] RUNNING BASE SETUP"
 sh arch-install/base.sh
 source arch-install/base.sh
-arch-chroot /mnt cd ~/
-arch-chroot /mnt /usr/bin/runuser -u $usrname -- git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd ..
+arch-chroot /mnt /usr/bin/runuser -u $usrname -- cd ~/ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd ..
 arch-chroot /mnt yay -S timeshift --needed
 echo    "--------------------------------------------------------"
-echo    "=== Membuat Restore Point"
+echo    "=== Membuat Restore Point 1"
 echo    "--------------------------------------------------------"
 timeshift --create --comments "Awal Install [BASE INSTALL ONLY]"
 echo    "-]-]-]-]-]-]-]-]-]-]-] RUNNING USER SETUP"
@@ -15,6 +14,6 @@ arch-chroot /mnt /usr/bin/runuser -u $usrname -- /home/$usrname/arch-install/1-u
 echo    "-]-]-]-]-]-]-]-]-]-]-] RUNNING ROOT SETUP"
 arch-chroot /mnt sh arch-install/2-root-setup.sh
 echo    "--------------------------------------------------------"
-echo    "=== Membuat Restore Point"
+echo    "=== Membuat Restore Point 2"
 echo    "--------------------------------------------------------"
 timeshift --create --comments "Awal Install [BASE INSTALL + PACKAGES & CONFIGURATION]"
