e8b1a8475943047b3033e85556dbf19dd93b6e66
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-14 19:54:47 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 sh arch-install/1-setup.sh
+echo    "--------------------------------------------------------"
+echo    "=== Membuat Restore Point"
+echo    "--------------------------------------------------------"
+timeshift --create --comments "Awal Install [BASE INSTALL ONLY]"
 source arch-install/base.sh
 arch-chroot /mnt /usr/bin/runuser -u $usrname -- /home/$usrname/arch-install/2-user-setup.sh
 arch-chroot /mnt sh arch-install/3-root-setup.sh
