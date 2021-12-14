f717ca531ed606ed97813dda18398a62119761ac
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-14 20:23:20 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,14 @@ clear
 chmod +x arch-install/*
 echo    "-]-]-]-]-]-]-]-]-]-]-] RUNNING BASE SETUP"
 sh arch-install/base.sh
+source arch-install/base.sh
+arch-chroot /mnt cd ~/
+arch-chroot /mnt /usr/bin/runuser -u $usrname -- git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd ..
+arch-chroot /mnt yay -S timeshift --needed
 echo    "--------------------------------------------------------"
 echo    "=== Membuat Restore Point"
 echo    "--------------------------------------------------------"
 timeshift --create --comments "Awal Install [BASE INSTALL ONLY]"
-source arch-install/base.sh
 echo    "-]-]-]-]-]-]-]-]-]-]-] RUNNING USER SETUP"
 arch-chroot /mnt /usr/bin/runuser -u $usrname -- /home/$usrname/arch-install/1-user-setup.sh
 echo    "-]-]-]-]-]-]-]-]-]-]-] RUNNING ROOT SETUP"
