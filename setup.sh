b7d5000defb48e1e0a9a5e0b93ddb0d7d58db9ad
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-14 20:06:17 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 clear
 chmod +x arch-install/*
+echo    "-]-]-]-]-]-]-]-]-]-]-] RUNNING BASE SETUP"
 sh arch-install/base.sh
 echo    "--------------------------------------------------------"
 echo    "=== Membuat Restore Point"
 echo    "--------------------------------------------------------"
 timeshift --create --comments "Awal Install [BASE INSTALL ONLY]"
 source arch-install/base.sh
-arch-chroot /mnt /usr/bin/runuser -u $usrname -- /home/$usrname/arch-install/2-user-setup.sh
-arch-chroot /mnt sh arch-install/3-root-setup.sh
+echo    "-]-]-]-]-]-]-]-]-]-]-] RUNNING USER SETUP"
+arch-chroot /mnt /usr/bin/runuser -u $usrname -- /home/$usrname/arch-install/1-user-setup.sh
+echo    "-]-]-]-]-]-]-]-]-]-]-] RUNNING ROOT SETUP"
+arch-chroot /mnt sh arch-install/2-root-setup.sh
+echo    "--------------------------------------------------------"
+echo    "=== Membuat Restore Point"
+echo    "--------------------------------------------------------"
+timeshift --create --comments "Awal Install [BASE INSTALL + PACKAGES & CONFIGURATION]"
