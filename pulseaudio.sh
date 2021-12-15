18f4763955299a742b09c920fb842361b77a32df
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-15 19:23:41 +0700

    Update pulseaudio.sh

--- pulseaudio.sh
+++ pulseaudio.sh
@@ -1,2 +1,2 @@
-sudo pacman -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-zeroconf lsp-plugins pulsemixer
+sudo pacman -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-zeroconf lsp-plugins pulsemixer --noconfirm --needed
 flatpak install com.github.wwmm.pulseeffects
