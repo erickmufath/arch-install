470ea04b927ec1d0551833df430d73ed8fd14848
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-15 12:15:14 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ arch-chroot /mnt yay -S timeshift --needed
 echo    "--------------------------------------------------------"
 echo    "=== Membuat Restore Point 1"
 echo    "--------------------------------------------------------"
-timeshift --create --comments "Awal Install [BASE INSTALL ONLY]"
+arch-chroot /mnt timeshift --create --comments "Awal Install [BASE INSTALL ONLY]"
 echo    "-]-]-]-]-]-]-]-]-]-]-] RUNNING USER SETUP"
 arch-chroot /mnt /usr/bin/runuser -u $usrname -- /home/$usrname/arch-install/1-user-setup.sh
 echo    "-]-]-]-]-]-]-]-]-]-]-] RUNNING ROOT SETUP"
@@ -16,4 +16,4 @@ arch-chroot /mnt sh arch-install/2-root-setup.sh
 echo    "--------------------------------------------------------"
 echo    "=== Membuat Restore Point 2"
 echo    "--------------------------------------------------------"
-timeshift --create --comments "Awal Install [BASE INSTALL + PACKAGES & CONFIGURATION]"
+arch-chroot /mnt timeshift --create --comments "Awal Install [BASE INSTALL + PACKAGES & CONFIGURATION]"
