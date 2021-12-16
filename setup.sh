4424e45558d43fb302c88dd1c35dca898ed314d0
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-16 23:23:24 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ echo    "-]-]-]-]-]-]-]-]-]-]-] RUNNING USER SETUP"
 echo -e "==================================================="
 echo    "=            Menginstall User Setup...            ="
 echo -e "==================================================="
-arch-chroot /mnt /usr/bin/runuser -u ${usrname} -- /home/$usrname/arch-install/1-user-setup.sh
+arch-chroot /mnt /usr/bin/runuser -u ${usrname} -- arch-install/1-user-setup.sh
 echo    "-]-]-]-]-]-]-]-]-]-]-] RUNNING ROOT SETUP"
 echo -e "==================================================="
 echo    "=            Menginstall Root Setup...            ="
