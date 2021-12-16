97de3dbccb5cc6da5dbc7cac50db471b4052d274
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-16 22:35:40 +0700

    Update 1-user-setup.sh

--- 1-user-setup.sh
+++ 1-user-setup.sh
@@ -1,20 +1,18 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 clear
+source arch-install/install.conf
 echo    "--------------------------------------------------------"
-echo    "=] 1. Pulseaudio [Default Kebanyakan Distro]"
-echo    "=] 2. Pipewire [Audio Controller Modern]"
+echo    "=] 1. Pulseaudio [X11/Wayland]"
+echo    "=] 2. Pipewire [Wayland]"
 echo    "--------------------------------------------------------"
 read -p "->] Pilih Audio Controller (1/2) : " audioc
-sh arch-install/extra-pkgs.sh
-sh arch-install/list-pkgs.sh
+arch-chroot /mnt /usr/bin/runuser -u ${usrname} -- /home/$usrname/arch-install/extra-pkgs.sh
+arch-chroot /mnt /usr/bin/runuser -u ${usrname} -- /home/$usrname/arch-install/list-pkgs.sh
 case $audioc in
 1)
-sh arch-install/pulseaudio.sh
+arch-chroot /mnt /usr/bin/runuser -u ${usrname} -- /home/$usrname/arch-install/pulseaudio.sh
 ;;
 2)
-sh arch-install/pipewire.sh
+arch-chroot /mnt /usr/bin/runuser -u ${usrname} -- /home/$usrname/arch-install/pipewire.sh
 ;;
 esac
-echo    "--------------------------------------------------------"
-echo    "== Langkah Terakhir Jalankan root-setup"
-echo    "--------------------------------------------------------"
