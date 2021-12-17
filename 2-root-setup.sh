3a95b77dacaf41b06e84028fb3b256de13471f7e
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-17 11:20:44 +0700

    Update 2-root-setup.sh

--- 2-root-setup.sh
+++ 2-root-setup.sh
@@ -1,23 +1,23 @@
 rm -rf /mnt/arch-install
 echo    "--------------------------------------------------------"
 echo -e "            Enabling Login Display Manager"
-sed -i 's/^# MAX_SIZE/MAX_SIZE/' /etc/default/zramd
-sed -i 's/^8192/${zramd}/' /etc/default/zramd
-systemctl enable sddm.service
+arch-chroot /mnt sed -i 's/^# MAX_SIZE/MAX_SIZE/' /etc/default/zramd
+arch-chroot /mnt sed -i 's/^8192/${zramd}/' /etc/default/zramd
+arch-chroot /mnt systemctl enable sddm.service
 echo -e "\nSetup SDDM Theme"
-cat <<EOF > /etc/sddm.conf
+cat <<EOF > /mnt/etc/sddm.conf
 [Theme]
 Current=Nordic
 EOF
-systemctl enable cups
-systemctl enable zramd
-systemctl enable ntpd.service
-systemctl disable dhcpcd.service
-systemctl stop dhcpcd.service
-systemctl enable NetworkManager.service
-systemctl enable bluetooth
-echo    "--------------------------------------------------------"
-cp -rf arch-install/xorg.conf /etc/X11/
+arch-chroot /mnt systemctl enable cups
+arch-chroot /mnt systemctl enable zramd
+arch-chroot /mnt systemctl enable ntpd.service
+arch-chroot /mnt systemctl disable dhcpcd.service
+arch-chroot /mnt systemctl stop dhcpcd.service
+arch-chroot /mnt systemctl enable NetworkManager.service
+arch-chroot /mnt systemctl enable bluetooth
+arch-chroot /mnt echo    "--------------------------------------------------------"
+cp -rf arch-install/xorg.conf /mnt/etc/X11/
 echo    "--------------------------------------------------------"
 echo    "=== Membuat Restore Point 2"
 echo    "--------------------------------------------------------"
