e8947a2bec3d9195c6f8330b2f2bb92c098c2962
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-17 10:31:54 +0700

    Update 1-user-setup.sh

--- 1-user-setup.sh
+++ 1-user-setup.sh
@@ -6,13 +6,13 @@ echo    "=] 1. Pulseaudio [X11/Wayland]"
 echo    "=] 2. Pipewire [Wayland]"
 echo    "--------------------------------------------------------"
 read -p "->] Pilih Audio Controller (1/2) : " audioc
-arch-chroot /mnt /usr/bin/runuser -u ${usrname} -- /home/$usrname/arch-install/extra-pkgs.sh
-arch-chroot /mnt /usr/bin/runuser -u ${usrname} -- /home/$usrname/arch-install/list-pkgs.sh
+arch-chroot /mnt /usr/bin/runuser -u ${usrname} -- arch-install/extra-pkgs.sh
+arch-chroot /mnt /usr/bin/runuser -u ${usrname} -- arch-install/list-pkgs.sh
 case $audioc in
 1)
-arch-chroot /mnt /usr/bin/runuser -u ${usrname} -- /home/$usrname/arch-install/pulseaudio.sh
+arch-chroot /mnt /usr/bin/runuser -u ${usrname} -- arch-install/pulseaudio.sh
 ;;
 2)
-arch-chroot /mnt /usr/bin/runuser -u ${usrname} -- /home/$usrname/arch-install/pipewire.sh
+arch-chroot /mnt /usr/bin/runuser -u ${usrname} -- arch-install/pipewire.sh
 ;;
 esac
