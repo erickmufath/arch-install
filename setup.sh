b6466e3b36e307db9f0b49494484cae68e9ef986
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-17 15:10:11 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 chmod +x arch-install/*
 sh arch-install/base.sh
+read -p "Enter Untuk Melanjutkan... " go
 echo -e "==================================================="
 echo    "=            Menginstall User Setup...            ="
 echo -e "==================================================="
 sh arch-install/1-user-setup.sh
+read -p "Enter Untuk Melanjutkan... " go
 echo -e "==================================================="
 echo    "=            Menginstall Root Setup...            ="
 echo -e "==================================================="
