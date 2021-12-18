0ea14ff6ab86d68cefbff0c991a3bc11565402bf
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-18 08:45:08 +0700

    Update pulseaudio.sh

--- pulseaudio.sh
+++ pulseaudio.sh
@@ -1,13 +1,15 @@
-yay -Rnsc pipewire --noconfirm
-yay -Rnsc pipewire-alsa --noconfirm
-yay -Rnsc pipewire-pulse --noconfirm
-yay -Rnsc pipewire-jack --noconfirm
-yay -Rnsc wireplumber --noconfirm
-yay -Rnsc gst-plugin-pipewire --noconfirm
-yay -Rnsc libpipewire02 --noconfirm
-yay -Rnsc pipewire-zeroconf --noconfirm
-yay -Rnsc lib32-pipewire --noconfirm
-yay -Rnsc lib32-pipewire-jack --noconfirm
-yay -Rnsc easyeffects --noconfirm 
+ if pacman -Qqe | grep -E "pipewire|easyeffects"; then
+    yay -Rnsc pipewire --noconfirm
+    yay -Rnsc pipewire-alsa --noconfirm
+    yay -Rnsc pipewire-pulse --noconfirm
+    yay -Rnsc pipewire-jack --noconfirm
+    yay -Rnsc wireplumber --noconfirm
+    yay -Rnsc gst-plugin-pipewire --noconfirm
+    yay -Rnsc libpipewire02 --noconfirm
+    yay -Rnsc pipewire-zeroconf --noconfirm
+    yay -Rnsc lib32-pipewire --noconfirm
+    yay -Rnsc lib32-pipewire-jack --noconfirm
+    yay -Rnsc easyeffects --noconfirm
+fi
 sudo pacman -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-zeroconf lsp-plugins pulsemixer --noconfirm --needed
 flatpak install com.github.wwmm.pulseeffects
