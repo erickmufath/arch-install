dad8e74e60bddacf55a3b866913c511fc74d0480
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-18 08:42:32 +0700

    Update pipewire.sh

--- pipewire.sh
+++ pipewire.sh
@@ -2,7 +2,7 @@ if pacman -Qqe | grep -E "pulseaudio|pulsemixer|pulseeffects"; then
     sudo pacman -Rnsc pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-zeroconf --noconfirm
 flatpak remove com.github.wwmm.pulseeffects
 fi
-sudo pacman -S lsp-plugins pulsemixer
+sudo pacman -S lsp-plugins pulsemixer --noconfirm --needed
 yay -S pipewire --noconfirm --needed
 yay -S pipewire-alsa --noconfirm --needed
 yay -S pipewire-pulse --noconfirm --needed
