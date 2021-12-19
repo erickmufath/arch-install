59ace1a52ca48a45ba8f265770ba8a87924d0696
erickmufath <90117993+erickmufath@users.noreply.github.com>
2021-12-19 11:35:35 +0700

    Update base.sh

--- base.sh
+++ base.sh
@@ -90,7 +90,6 @@ echo "usrname=$usrname" >> arch-install/install.conf
 echo "zramd=$zramd" >> arch-install/install.conf
 cp -rf arch-install /mnt/home/"${usrname}"
 cp -rf arch-install /mnt
-arch-chroot /mnt pacman -S xf86-video-intel xf86-video-amdgpu xf86-video-ati xf86-video-vesa --noconfirm
 case $boot in
 1)
 arch-chroot /mnt pacman -Sy grub os-prober --noconfirm
